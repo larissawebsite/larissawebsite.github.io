2d1d23b5b3420ae7960b26650d73241befbb75a6
larissawebsite <107081204+larissawebsite@users.noreply.github.com>
2022-06-07 19:38:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const { MessageButton, MessageActionRow } = require('discord.js');
-const { MessageEmbed } = require('discord.js');
-const Discord = require("discord.js");
 
-
-module.exports = {
-   name: "larissa",
-
-  run: async (client, message, args) => {
-
-let embed = new MessageEmbed()
-.setDescription(`Larissa
-
-Olá me chamo Larissa, prazer em te conhecer!
-Me adicione em seu servidor.`)
-.setAuthor(`https://imgur.com/jaGKJis`)
-message.channel.send(({embeds: [embed]}))
- }
-} 
