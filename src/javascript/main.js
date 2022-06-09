0487c9d3ba8e5764da94a9a842ba74ac06548e17
larissawebsite <107081204+larissawebsite@users.noreply.github.com>
2022-06-09 19:11:18 -0300

    Add files via upload

---
+++ src/javascript/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.nav-bar-toggle').on('click', function(){
+      if ($('.nav-bar-links').hasClass('active')){
+        $('.nav-bar-links').removeClass('active');
+      } else {
+        $('.nav-bar-links').addClass('active');
+      }
+    })
+  });
+  
